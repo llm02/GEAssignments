fcd38c229ca55c28b38260fd3f55b5cf73fd5e25
Alparse <serhatalpar@yahoo.com>
2017-02-16 21:20:14 -0600

    test3

--- src/lessonExamples/Car.java
+++ src/lessonExamples/Car.java
@@ -23,7 +23,7 @@ package lessonExamples;
 	//(including subclasses). The visibility of private members extends to nested classes.
 	// In Object Oriented Programming we use this methodology to "encapsulate" the details
 	// of how a class operates so that it can be more easily coupled to other elements of code
-//test2
+//test3
 
 public class Car {
 	//Below are the Car class VARIABLES;
